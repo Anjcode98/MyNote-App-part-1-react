555b7a2814f753eb2b18f794d2b31e404f129c70
Anjana Weerarathne <74248458+Anjcode98@users.noreply.github.com>
2025-02-21 14:49:23 +0530

    Add files via upload

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Note() {
+  return (
+    <div  className='note'>
+      <h1> This is Title</h1>
+      <h1> This is content of note.</h1>
+    </div>
+  );
+}
+
+export default Note;
